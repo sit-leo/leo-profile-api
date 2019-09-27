0378bed35246a9ca4cbf0d91a09e001c863d5afc
Keerati <keerati.jear@gmail.com>
2019-09-27 21:45:35 +0700

    [ADD] create and implement web mvc config for token

---
+++ src/main/java/app/leo/profile/WebMvcConfig.java
@@ -0,0 +1,40 @@
+package app.leo.profile;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+import app.leo.matching.interceptors.TokenInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(mappedInterceptor());
+	}
+
+	@Bean
+	public MappedInterceptor mappedInterceptor() {
+		return new MappedInterceptor(
+				new String[] {"/**", ""},
+				new String[] {"/actuator/**", "/error/**"},
+				tokenInterceptor());
+	}
+
+	@Bean
+	public TokenInterceptor tokenInterceptor() {
+		return new TokenInterceptor();
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry
+				.addMapping("/**")
+				.allowedHeaders("*")
+				.allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*");
+	}
+}
